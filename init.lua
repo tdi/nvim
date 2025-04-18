405e7db1da85665ed96f28f24b4dc8f4c6bd88ca
Darek Dwornikowski <dariusz.dwornikowski@gmail.com>
2025-04-18 12:38:59 +0200

    added trouble

--- init.lua
+++ init.lua
@@ -587,16 +587,20 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         -- gopls = {},
+        ruff = {
+          settings = {},
+        },
         pyright = {
           settings = {
             python = {
               analysis = {
-                typeCheckingMode = 'basic',
+                typeCheckingMode = 'off',
                 useLibraryCodeForTypes = true,
               },
             },
           },
         },
+
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
@@ -949,6 +953,43 @@ require('lazy').setup({
           file_types = { 'markdown', 'Avante' },
         },
         ft = { 'markdown', 'Avante' },
+      },
+    },
+  },
+  {
+    'folke/trouble.nvim',
+    opts = {}, -- for default options, refer to the configuration section for custom setup.
+    cmd = 'Trouble',
+    keys = {
+      {
+        '<leader>xx',
+        '<cmd>Trouble diagnostics toggle<cr>',
+        desc = 'Diagnostics (Trouble)',
+      },
+      {
+        '<leader>xX',
+        '<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
+        desc = 'Buffer Diagnostics (Trouble)',
+      },
+      {
+        '<leader>cs',
+        '<cmd>Trouble symbols toggle focus=false<cr>',
+        desc = 'Symbols (Trouble)',
+      },
+      {
+        '<leader>cl',
+        '<cmd>Trouble lsp toggle focus=false win.position=right<cr>',
+        desc = 'LSP Definitions / references / ... (Trouble)',
+      },
+      {
+        '<leader>xL',
+        '<cmd>Trouble loclist toggle<cr>',
+        desc = 'Location List (Trouble)',
+      },
+      {
+        '<leader>xQ',
+        '<cmd>Trouble qflist toggle<cr>',
+        desc = 'Quickfix List (Trouble)',
       },
     },
   },
