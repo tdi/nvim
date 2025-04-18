7dc34a9ca55db52b13027607d3a5ec2baff3ade9
Darek Dwornikowski <dariusz.dwornikowski@gmail.com>
2025-04-18 20:36:34 +0200

    plugins

---
+++ lua/custom/plugins/noice.lua
@@ -0,0 +1,30 @@
+return {
+  {
+    'folke/noice.nvim',
+    event = 'VeryLazy',
+    lsp = {
+      override = {
+        ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+        ['vim.lsp.util.stylize_markdown'] = true,
+        ['cmp.entry.get_documentation'] = true,
+      },
+    },
+    presets = {
+      bottom_search = true,
+      command_palette = true,
+      long_message_to_split = true,
+      lsp_doc_border = false,
+    },
+    opts = {
+      -- add any options here
+    },
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      'MunifTanjim/nui.nvim',
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      'rcarriga/nvim-notify',
+    },
+  },
+}
