65deb9c15c8e1e3912a3582aae57655edd9c3925
Darek Dwornikowski <dariusz.dwornikowski@gmail.com>
2025-04-20 16:34:10 +0200

    split mapping

--- init.lua
+++ init.lua
@@ -114,6 +114,9 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
 -- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
 
+vim.keymap.set('n', '<C-w>|', ':vsplit<CR>', { desc = 'Split vertical' })
+vim.keymap.set('n', '<C-w>-', ':split<CR>', { desc = 'Split vertical' })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
