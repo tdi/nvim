12ea073212b95751a52dbc60823b474b6da15359
Darek Dwornikowski <dariusz.dwornikowski@gmail.com>
2026-01-17 19:42:43 +0100

    Add yaml etc

--- init.lua
+++ init.lua
@@ -601,6 +601,19 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         gopls = {},
+        yamlls = {
+          settings = {
+            yaml = {
+              schemas = {
+                ['https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/v1.18.0-standalone-strict/all.json'] = '/*.k8s.yaml',
+                kubernetes = { '/*.yaml', '/*.yml' },
+              },
+              validate = true,
+              completion = true,
+              hover = true,
+            },
+          },
+        },
         ruff = {
           settings = {},
         },
