6ad141f9d1f35d2e9b6d87f77e83ea6aec33f84c
Darek Dwornikowski <dariusz.dwornikowski@gmail.com>
2026-04-02 12:46:35 +0200

    remove fiddget

--- init.lua
+++ init.lua
@@ -435,9 +435,6 @@ require('lazy').setup({
       'williamboman/mason-lspconfig.nvim',
       'WhoIsSethDaniel/mason-tool-installer.nvim',
 
-      -- Useful status updates for LSP.
-      { 'j-hui/fidget.nvim', opts = {} },
-
       -- Allows extra capabilities provided by blink.cmp
       'saghen/blink.cmp',
     },
